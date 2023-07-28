adb73e56cb6b1316ddf58e86790e17c9f2cd03c7
panthersinsights-fla <panthersinsights@floridapanthers.com>
2023-07-28 15:23:30 -0400

    fla_teams update

--- catnip/fla_teams.py
+++ catnip/fla_teams.py
@@ -5,17 +5,11 @@ class FLA_Teams(BaseModel):
 
     webhook: str 
 
-    _my_message: connectorcard
-
-    class Config:
-        underscore_attrs_are_private = True
-
-    def __init__(self, **data):
-
-        super().__init__(**data)
-        self._my_message = connectorcard(self.webhook)
-
-
+    @property
+    def _my_message(self) -> connectorcard:
+        return connectorcard(self.webhook)
+    
+    
     def send_message(self, body_text: str) -> None:
 
         self._my_message.text(body_text)
