90379902f2143d334bc7d4772f60ba0edd6a6cad
panthersinsights-fla <panthersinsights@floridapanthers.com>
2023-09-07 10:40:23 -0400

    seatgeek manifests update

--- catnip/fla_seatgeek.py
+++ catnip/fla_seatgeek.py
@@ -223,7 +223,13 @@ class FLA_SeatGeek(BaseModel):
                 return None
 
         elif endpoint == "manifests":
-            return None 
+
+            if response['data']:   
+                response['data'] = [{k[1:] if k.startswith('_') else k.replace('"',''): v for k, v in d.items()} for d in response['data']]
+                return DataFrame[self.input_schema](response['data'])
+            
+            else:
+                return None
 
         elif endpoint == "payments":
 
