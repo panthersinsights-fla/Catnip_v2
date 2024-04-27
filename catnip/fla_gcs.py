f0964d045e396e29ad1922704abe07b9249c3a04
panthersinsights-fla <panthersinsights@floridapanthers.com>
2024-04-27 13:41:12 -0400

    fla gcs testing

--- catnip/fla_gcs.py
+++ catnip/fla_gcs.py
@@ -13,6 +13,7 @@ class FLA_Gcs(BaseModel):
     output_schema: DataFrameModel = None 
 
     def df_to_gcs(
+        self,
         df: pd.DataFrame,
         file_name: str,
         bucket_path: str,
@@ -24,6 +25,10 @@ class FLA_Gcs(BaseModel):
         suffix = ".csv" if as_csv else ""
         full_path = f"gs://{bucket_path}/{file_name}{suffix}"
 
+        # validate
+        if self.output_schema:
+            self.output_schema.validate(df)
+        
         # upload
         df.to_csv(full_path, index = with_index)
 
