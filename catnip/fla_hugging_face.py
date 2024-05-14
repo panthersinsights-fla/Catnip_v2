6296fb636ee97d9fddc21d7d96416221af650e3d
panthersinsights-fla <panthersinsights@floridapanthers.com>
2024-05-14 13:22:31 -0400

    hugging face testing

---
+++ catnip/fla_hugging_face.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, SecretStr
+from typing import Dict
+from catnip.fla_requests import FLA_Requests
+import httpx
+
+class FLA_Hugging_Face(BaseModel):
+
+    api_token: SecretStr
+    model_id: str  # i.e. facebook/bart-large-mnli
+
+    @property
+    def _headers(self) -> Dict:
+        return {"Authorization": f"Bearer {self.api_token.get_secret_value()}"}
+    
+    @property
+    def _base_url(self) -> str:
+        return f"https://api-inference.huggingface.co/models/{self.model_id}"
+
+    
+    def query_model(self, payload: Dict) -> httpx.Response:
+
+        with FLA_Requests().create_session() as session:
+
+            response = session.post(
+                url = self._base_url,
+                headers = self._headers,
+                json = payload
+            )
+
+        return response
