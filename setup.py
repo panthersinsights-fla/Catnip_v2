06eb9dd032e766a7dcb3a4874661c3f66a70c83b
panthersinsights-fla <panthersinsights@floridapanthers.com>
2025-03-02 18:31:11 -0500

    gameday testing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = "catnip_v2",
-    version = "0.0.369",
+    version = "0.0.370",
     author = "Panthers Insights",
     author_email = "panthersinsights@floridapanthers.com",
     description = "Facilitating repeatable internal code",
