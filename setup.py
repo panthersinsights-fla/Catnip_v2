95af1aab617346678d777e5c3a8ebabf0a17703a
mcgoyen@floridapanthers.com <mcgoyen@floridapanthers.com>
2025-07-11 13:09:32 -0400

    seatgeek update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = "catnip_v2",
-    version = "0.0.462",
+    version = "0.0.463",
     author = "Panthers Insights",
     author_email = "panthersinsights@floridapanthers.com",
     description = "Facilitating repeatable internal code",
